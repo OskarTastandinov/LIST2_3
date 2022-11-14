aefa9503ad0fc1fdd34a2caccfd161c527731b8b
askar <tashtandinov.oskar@yandex.ru>
2022-11-14 23:43:12 +0700

    Коммит по супермаркету

---
+++ Supermarket.java
@@ -0,0 +1,11 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.List;
+import java.util.Random;
+
+public class Supermarket {
+    public static void main(String [] args){
+        System.out.println("Hello Ira");
+    }
+}
